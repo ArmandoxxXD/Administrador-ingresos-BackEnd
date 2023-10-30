bd4919fbabd356c800f9b4910634485ad998fe9e
ArmandoxxXD <mamojose58@gmail.com>
2023-10-29 18:59:38 -0600

    Corregi bugs al procesar exel y cambie un poco de logica

--- Componentes/Ingresos/services.js
+++ Componentes/Ingresos/services.js
@@ -3,60 +3,59 @@ const fs = require('fs');
 const { pool } = require('../../index'); 
 
 async function procesarArchivoExcel(filePath) {
-  const workbook = XLSX.readFile(filePath);
-  const sheetName = workbook.SheetNames[0];
-  const sheet = workbook.Sheets[sheetName];
-
-  // Puedes acceder a las celdas específicas o a rangos de celdas aquí
-  const inscripciones_total_mes = sheet['C18'].v; 
-  const titulos_total_mes = sheet['C19'].v;
-  const subvenciones_total_mes = sheet['C20'].v;
-  const ventaProductos_total_mes = sheet['C21'].v;
-  const auxiliares_total_mes = sheet['C22'].v;
-  const total_mes = sheet['C23'].v;
-
-  const Dias = {
-    s: { c: 1, r: 27 }, // Inicio en B28 (B es la segunda columna, por lo que c=1)
-    e: { c: 1, r: 57 }, // Fin en B58
-  };
-
-  const datosDelRangoDias = XLSX.utils.sheet_to_json(sheet, { header: 1, defval: '', range: Dias });
-
-  // Formatea las fechas a partir de los números almacenados en Excel
-  const fechaFormat = 'dd/mm/yyyy';
-  datosDelRangoDias.forEach((fila) => {
-    const valorNumerico = fila[0];
-    if (!isNaN(valorNumerico)) {
-      const fecha = new Date((valorNumerico - 25568) * 86400 * 1000); // 25569 es un ajuste para fechas en Excel
-      fila[0] = fecha.toLocaleDateString('es-ES', { dateFormat: fechaFormat }); // Formatea la fecha
-    }
-  });
-
-  const Total_por_Dias = {
-    s: { c: 7, r: 27 }, // Inicio en B28 (B es la segunda columna, por lo que c=1)
-    e: { c: 7, r: 57 }, // Fin en B58
-  };
-
-  const datosDelRangoTotalDia = XLSX.utils.sheet_to_json(sheet, { header: 1, defval: '', range: Total_por_Dias });
-
-  fs.unlink(filePath, (err) => {
-    if (err) {
-      console.error(`Error al eliminar el archivo temporal: ${err}`);
-    }
-  });
+    try {
+      const workbook = XLSX.readFile(filePath);
+      const sheetName = workbook.SheetNames[0];
+      const sheet = workbook.Sheets[sheetName];
   
-  return {
-    DatosMensuales:{
-    inscripciones_total_mes,
-    titulos_total_mes,
-    subvenciones_total_mes,
-    ventaProductos_total_mes,
-    auxiliares_total_mes,
-    total_mes
-    },
-    Diario:{datosDelRangoDias,datosDelRangoTotalDia}
-  };
-}
+      // Verifica si las celdas esperadas existen
+      if (!sheet['C18'] || !sheet['C19'] || !sheet['C20'] || !sheet['C21'] || !sheet['C22'] || !sheet['C23']) {
+        throw new Error('Formato de archivo inválido o celdas esperadas faltantes.');
+      }
+  
+      const inscripciones_total_mes = sheet['C18'].v;
+      const titulos_total_mes = sheet['C19'].v;
+      const subvenciones_total_mes = sheet['C20'].v;
+      const ventaProductos_total_mes = sheet['C21'].v;
+      const auxiliares_total_mes = sheet['C22'].v;
+      const total_mes = sheet['C23'].v;
+  
+      const fechaFormat = 'dd/mm/yyyy';
+      let datosDelRangoDias = [];
+      let datosDelRangoTotalDia = [];
+      for (let i = 28; i <= 58; i++) {
+        const fechaCell = sheet[`B${i}`];
+        const totalCell = sheet[`H${i}`];
+        if (fechaCell && totalCell) {
+          const valorNumerico = fechaCell.v;
+          const fecha = new Date((valorNumerico - 25568) * 86400 * 1000); 
+          datosDelRangoDias.push(fecha.toLocaleDateString('es-ES', { dateFormat: fechaFormat }));
+          datosDelRangoTotalDia.push(totalCell.v);
+        }
+      }
+  
+      return {
+        DatosMensuales: {
+          inscripciones_total_mes,
+          titulos_total_mes,
+          subvenciones_total_mes,
+          ventaProductos_total_mes,
+          auxiliares_total_mes,
+          total_mes
+        },
+        Diario: { datosDelRangoDias, datosDelRangoTotalDia }
+      };
+    } catch (error) {
+      console.error(`Error al procesar el archivo: ${error.message}`);
+      throw error;
+    } finally {
+      fs.unlink(filePath, (err) => {
+        if (err) {
+          console.error(`Error al eliminar el archivo temporal: ${err}`);
+        }
+      });
+    }
+  }
 
 async function insertarDatos({ reporteDiario, reporteMensual, fechaReporte }) {
   const client = await pool.connect();
@@ -80,7 +79,7 @@ async function insertarDatos({ reporteDiario, reporteMensual, fechaReporte }) {
               INSERT INTO sub_ingreso (ingreso_id, fecha, total)
               VALUES ($1, $2, $3);
           `;
-          const subIngresoValues = [ingresoId, reporteDiario.fechas[i][0], reporteDiario.totales[i][0]];
+          const subIngresoValues = [ingresoId, reporteDiario.fechas[i], reporteDiario.totales[i]];
           await client.query(insertSubIngresoQuery, subIngresoValues);
       }
       
@@ -101,7 +100,7 @@ async function obtenerReportePorRangoFechas(fechaInicio, fechaFin) {
   try {
       const query = `
           SELECT * FROM ingreso 
-          WHERE fecha BETWEEN $1 AND $2
+          WHERE to_char(fecha, 'YYYY-MM') BETWEEN $1 AND $2
           ORDER BY fecha ASC;  -- Esto ordenará los reportes por fecha en orden ascendente
       `;
       const result = await client.query(query, [fechaInicio, fechaFin]);
@@ -238,6 +237,27 @@ async function getSumaTotalMesIngresos() {
   }
 }
 
+async function getSumaTotalMesIngresosPorMes(fechaInicio, fechaFin) {	
+    const client = await pool.connect();
+  
+    try {
+        const query = `
+            SELECT SUM((data->>'total_mes')::integer) AS suma_total_mes 
+            FROM ingreso
+            WHERE to_char(fecha, 'YYYY-MM') BETWEEN $1 AND $2
+        `;
+        const result = await client.query(query, [fechaInicio, fechaFin]);
+        
+        // Retorna la suma
+        return result.rows[0].suma_total_mes;
+    } catch (error) {
+        throw error;
+    } finally {
+        client.release();
+    }
+  }
+
+  
 module.exports = {
   procesarArchivoExcel,
   insertarDatos,
@@ -247,5 +267,6 @@ module.exports = {
   obtenerReporteDiarioPorId,
   eliminarReportePorId,
   validarFecha,
-  getSumaTotalMesIngresos
+  getSumaTotalMesIngresos,
+  getSumaTotalMesIngresosPorMes
 };
